8c2d1e10ee614d42394ef467a3f91fcd4124b3e5
ArenVosk <84827064+ArenVosk@users.noreply.github.com>
2021-09-07 17:48:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+menu_list_array = ["Veg Margharita Pizza", "Country Special Pizza", "Farmhouse Pizza", "Mexican Green Wave Pizza", "peppy Paneer Pizza", "Deluxe Veggie Pizza", "5 Pepper Pizza", "Veggie Paradise Pizza", "Spicy Triple Tango Pizza", "Veg Extravaganza Pizza", "Chef's Chicken Choice Pizza", "Cheese & BBQ Chicken Pizza", "Chicken Golden Delight Pizza", "Chicken Mexicana Pizza", "Barbaque Chicken Pizza", "Cheese & Pepperoni Pizza", "Chicken Dominator Pizza", "Non Veg Supreme Pizza"];
+
+function getmenu(){
+var htmldata;
+htmldata="<ol class='menulist'>"
+menu_list_array.sort();
+for(var i=0; i<menu_list_array.length; i++){
+    
+}
+}
